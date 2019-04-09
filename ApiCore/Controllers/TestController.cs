2d890865cd3e16d9df4af39adfd771e912bd4bb0
dalmenar <dalmenar@openfinance.es>
2019-04-09 12:34:53 +0200

    Hi !

---
+++ ApiCore/Controllers/TestController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RabbitMQ.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiCore.Controllers
+{
+    public class TestController :  Controller
+    {
+        public TestController()
+        {
+
+        }
+        // GET api/values
+        [HttpGet]
+        [Route("apiTest/suma/{cantidad}")]
+        public bool Suma(int cantidad)
+        {
+            ConnectionFactory factory = new ConnectionFactory()
+            {
+                HostName = "bulldog.rmq.cloudamqp.com",
+                UserName = "pcjuohcb",
+                Password = "qe7Fe_UP8Ym6DHrzxuzpGdKp9_cHVXuC",
+                Port = 1883,
+                VirtualHost = "pcjuohcb",
+                Endpoint = new AmqpTcpEndpoint(new Uri("amqp://pcjuohcb:qe7Fe_UP8Ym6DHrzxuzpGdKp9_cHVXuC@bulldog.rmq.cloudamqp.com/pcjuohcb"))
+            };
+
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "hello", durable: false, exclusive: false, autoDelete: false, arguments: null);
+
+                var body = Encoding.UTF8.GetBytes(cantidad.ToString());
+
+                channel.BasicPublish(exchange: "", routingKey: "hello", basicProperties: null, body: body);
+                
+
+            }
+            return true;
+        }
+    }
+}
---
+++ ConsoleCache/Program.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.DependencyInjection;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+
+namespace ConsoleCache
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var services = new ServiceCollection().AddMemoryCache()
+               .BuildServiceProvider();
+
+             // create a service provider from the service collection
+             
+
+            // resolve the dependency graph
+            var cacheService = services.GetService<IMemoryCache>();
+
+
+
+            Console.WriteLine("Hello World!");
+
+
+            var factory = new ConnectionFactory()
+            {
+                HostName = "bulldog.rmq.cloudamqp.com",
+                UserName = "pcjuohcb",
+                Password = "qe7Fe_UP8Ym6DHrzxuzpGdKp9_cHVXuC",
+                Port = 1883,
+                VirtualHost = "pcjuohcb",
+                Endpoint = new AmqpTcpEndpoint(new Uri("amqp://pcjuohcb:qe7Fe_UP8Ym6DHrzxuzpGdKp9_cHVXuC@bulldog.rmq.cloudamqp.com/pcjuohcb"))
+            };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "hello", durable: false, exclusive: false, autoDelete: false, arguments: null);
+
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += (model, ea) => {
+                    Console.WriteLine("Ha llegado bien...");
+                    var body = ea.Body;
+                    var cantidadStr = Encoding.UTF8.GetString(body);
+                    int cantidad;
+                    bool canParse =  int.TryParse(cantidadStr,out cantidad);
+
+                    if (canParse)
+                    {
+                        int micaja = 0;
+                        bool isExist = cacheService.TryGetValue("micaja", out micaja);
+                        var cacheEntryOptions = new MemoryCacheEntryOptions()
+                            .SetSlidingExpiration(TimeSpan.FromSeconds(5000));
+                        int suma = micaja + cantidad;
+                        Console.WriteLine("Ha llegado..." + cantidad);
+                        Console.WriteLine("Llevamos..." + suma);
+                        cacheService.Set("micaja", suma, cacheEntryOptions);
+                    }
+                };
+                channel.BasicConsume(queue: "hello", autoAck: true, consumer: consumer);
+
+                Console.WriteLine("Escuchando al conejo...");
+                while (true) ;
+
+            }
+        }
+
+        public static void WaitingMsg()
+        {
+            
+
+        }
+    }
+}

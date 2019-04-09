4d4d9c9344a5dea8927b63c91e3aa6a04f588e0f
dalmenar <dalmenar@openfinance.es>
2019-04-09 12:53:29 +0200

    Finish update .net cote 2.2

--- ApiCoreLog/Startup.cs
+++ ApiCoreLog/Startup.cs
@@ -34,10 +34,9 @@ namespace ApiCoreLog
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
-            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
-            loggerFactory.AddDebug();
-
-            app.UseMvc();
+            app.UseMvc(routes => {
+                routes.MapRoute("default", "{controller=values}/{action=get}/{id?}");
+            });
         }
     }
 }
